6f57941866a2c0762518203b28fae2b2815ebbdb
fbzr <fabricio.spb@gmail.com>
2020-03-20 08:29:56 -0700

    create action types

---
+++ smurfs/src/actions/types.js
@@ -0,0 +1,5 @@
+export const ERROR = 'ERROR';
+export const FETCH_DATA = 'FETCH_DATA';
+export const ADD_SMURF = 'ADD_SMURF';
+export const EDIT_SMURF = 'EDIT_SMURF';
+export const DELETE_SMURF = 'EDIT_SMURF';
