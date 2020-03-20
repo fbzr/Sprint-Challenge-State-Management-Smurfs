91f13cc6d4c9da26d1c6877121ffb34c88355bc7
fbzr <fabricio.spb@gmail.com>
2020-03-20 08:52:56 -0700

    dispatch fetchData from store

--- smurfs/src/store/store.js
+++ smurfs/src/store/store.js
@@ -2,10 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import smurfsReducer from '../reducers/smurfs';
+import { fetchData } from '../actions/smurfVillage';
 
 const store = createStore(
     smurfsReducer,
     composeWithDevTools(applyMiddleware(thunk))
 );
+
+store.dispatch(fetchData());
 
 export default store;
