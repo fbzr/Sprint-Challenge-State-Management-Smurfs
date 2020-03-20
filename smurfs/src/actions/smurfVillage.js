c827cd619dc2cae7805f1d9ff7247c31ee09f502
fbzr <fabricio.spb@gmail.com>
2020-03-20 08:38:00 -0700

    create action file with empty functions

---
+++ smurfs/src/actions/smurfVillage.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import {
+    FETCH_DATA,
+    ERROR,
+    ADD_SMURF,
+    EDIT_SMURF,
+    DELETE_SMURF
+} from './types';
+
+export const fetchData = () => async dispatch => {
+    
+}
+
+export const addSmurf = ({ name, age, height }) => async dispatch => {
+    
+}
+
+export const editSmurf = ({ name, age, height, id }) => async dispatch => {
+    
+}
+
+export const removeSmurf = smurfId => async dispatch => {
+    
+}
